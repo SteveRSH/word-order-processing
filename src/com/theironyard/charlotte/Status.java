f608a9bcaf47a19786baf0cb924724f38b7b7736
Ben Sterrett <raynjamin@gmail.com>
2017-09-26 15:09:46 -0400

    Intiial Commit

---
+++ src/com/theironyard/charlotte/Status.java
@@ -0,0 +1,8 @@
+package com.theironyard.charlotte;
+
+public enum Status {
+    INITIAL,
+    ASSIGNED,
+    IN_PROGRESS,
+    DONESKIES
+}
---
+++ src/com/theironyard/charlotte/WorkOrder.java
@@ -0,0 +1,94 @@
+package com.theironyard.charlotte;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/* WORKORDER JAVABEAN */
+public class WorkOrder {
+    private static int STATIC_ID;
+    private int id;
+    private String description;
+    private String senderName;
+    private Status status;
+
+    public WorkOrder() {
+    }
+
+    public WorkOrder(String description, String senderName) {
+        // set an id for the current work order.
+        this.id = STATIC_ID++;
+
+        // set the STATUS TO INITIAL
+        this.status = Status.INITIAL;
+
+        this.description = description;
+        this.senderName = senderName;
+    }
+
+    private void persist() {
+        ObjectMapper mapper = new ObjectMapper()
+
+        try {
+            // save object json string to disk.
+            File f = new File(this.id + ".jn");
+            FileWriter fw = new FileWriter(f);
+
+            // convert our object to json..
+            String objString = mapper.writeValueAsString(new WorkOrder()) + "pants";
+
+            // write this object, as json,
+            // to the file using our file writer.
+            fw.append(objString);
+
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "WorkOrder{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", senderName='" + senderName + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
